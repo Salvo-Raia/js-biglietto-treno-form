44bd37adfa7cdc39d04f7368f562cca5be39afb6
Salvo-Raia <salvo.raia@outlook.it>
2026-01-13 17:59:46 +0100

    Add innerText + Styling page

--- js/script.js
+++ js/script.js
@@ -4,29 +4,50 @@ const nameInput = document.getElementById("name-input");
 const kmInput = document.getElementById("km-input");
 const ageInput = document.getElementById("age-input")
 
-/* Ticket price variables */
-
+/* Final Ticket variables */
+const ticketOutput = document.getElementById("ticket-price");
+const passenger = document.getElementById("passenger-name"); 
+const plan = document.getElementById("plan");
+const price = document.getElementById("price");
 
 ticketForm.addEventListener("submit", function (event) {
 event.preventDefault(); 
+// Salvo gli input dell'utente // 
 const name = nameInput.value;
 const km = kmInput.valueAsNumber;
 const age = ageInput.value;
+// Tariffe // 
 const ticketStandard = km * 0.21; 
 const discountUnderAge = ticketStandard * 0.20;
 const discountOverAge = ticketStandard * 0.40;
 let finalTicketPrice;
+// Stampo gli input dell'utente //
+console.log("Dati form raccolti");
 console.log("Nome:", name);
 console.log("Chilometri:", km);
 console.log("Fascia d'età:", age);
+// Svuoto i campi del form // 
+nameInput.value="";
+kmInput.value="";
+ageInput.value="";
+
+ticketOutput.classList.remove("d-none")
 
 if (age === "Minorenne") {
     finalTicketPrice = (ticketStandard - discountUnderAge).toFixed(2); 
-    console.log(finalTicketPrice, "€");
+    passenger.innerText = name;
+    plan.innerText = "Sconto minorenni"
+    price.innerText = `${finalTicketPrice} €`
 } else if (age === "Over 65") {
     finalTicketPrice = (ticketStandard - discountOverAge).toFixed(2);
+    passenger.innerText = name;
+    plan.innerText = "Sconto anziani"
+    price.innerText = `${finalTicketPrice} €`
 } else {
     finalTicketPrice = ticketStandard.toFixed(2);
+    passenger.innerText = name;
+    plan.innerText = "Tariffa standard"
+    price.innerText = `${finalTicketPrice} €`
 }
 })
 
