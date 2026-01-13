9cebdb1aadc05312ecc3081c3e97d409fd612422
Salvo-Raia <salvo.raia@outlook.it>
2026-01-13 17:18:01 +0100

    Add ticket pricing logic

--- js/script.js
+++ js/script.js
@@ -4,27 +4,29 @@ const nameInput = document.getElementById("name-input");
 const kmInput = document.getElementById("km-input");
 const ageInput = document.getElementById("age-input")
 
-const name = nameInput.value;
-const km = kmInput.value;
-const age = ageInput.value;
+/* Ticket price variables */
 
 
 ticketForm.addEventListener("submit", function (event) {
 event.preventDefault(); 
 const name = nameInput.value;
-const km = kmInput.value;
+const km = parseFloat(kmInput.value);
 const age = ageInput.value;
-console.log(name);
-console.log(km);
-console.log(age);
+const ticketStandard = km * 0.21; 
+const discountUnderAge = ticketStandard * 0.20;
+const discountOverAge = ticketStandard * 0.40;
+let finalTicketPrice;
+console.log("Nome:", name);
+console.log("Chilometri:", km);
+console.log("Fascia d'età:", age);
+
+if (age === "Minorenne") {
+    finalTicketPrice = (ticketStandard - discountUnderAge).toFixed(2); 
+    console.log(finalTicketPrice, "€");
+} else if (age === "Over 65") {
+    finalTicketPrice = (ticketStandard - discountOverAge).toFixed(2);
+} else {
+    finalTicketPrice = ticketStandard.toFixed(2);
+}
 })
 
-
-
-
-
-/* Ticket price variables */
-// const ticketStandard = km * 0.21; 
-//const discountUnderAge = ticketStandard * 0.20;
-// const discountOverAge = ticketStandard * 0.40;
-// let finalTicketPrice;
