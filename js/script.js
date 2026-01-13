8f7c5cae38c7b94a139d77bd5835dafe0f0b6a1a
Salvo-Raia <salvo.raia@outlook.it>
2026-01-13 17:04:33 +0100

    Test form inputs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,30 @@
+/* Form variables */
 const ticketForm = document.getElementById("ticket-form");
+const nameInput = document.getElementById("name-input");
+const kmInput = document.getElementById("km-input");
+const ageInput = document.getElementById("age-input")
+
+const name = nameInput.value;
+const km = kmInput.value;
+const age = ageInput.value;
+
 
 ticketForm.addEventListener("submit", function (event) {
 event.preventDefault(); 
+const name = nameInput.value;
+const km = kmInput.value;
+const age = ageInput.value;
+console.log(name);
+console.log(km);
+console.log(age);
 })
+
+
+
+
+
+/* Ticket price variables */
+// const ticketStandard = km * 0.21; 
+//const discountUnderAge = ticketStandard * 0.20;
+// const discountOverAge = ticketStandard * 0.40;
+// let finalTicketPrice;
